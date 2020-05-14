77d58960884292993f0a614a63fd235701c5ba36
yarzem <yarsmirnov2404@gmail.com>
2020-05-14 21:09:16 +0300

    hookah

---
+++ cmake-build-debug/CMakeFiles/cravler.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cravler.dir/main.cpp.obj"
+  "cravler.exe"
+  "cravler.exe.manifest"
+  "cravler.pdb"
+  "libcravler.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cravler.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
